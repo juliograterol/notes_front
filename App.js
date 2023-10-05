f1b923b3b348f09489b5de62f7c0cba567903f57
Julio Graterol <juliograterolb@gmail.com>
2023-10-05 17:34:10 -0400

    first commit with simple view

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.logContainer}>
+        <Text>Iniciar Sesión</Text>
+        <View>
+          <TextInput style={styles.placeHold} placeholder="Usuario"></TextInput>
+          <TextInput
+            style={styles.placeHold}
+            placeholder="Contraseña"
+          ></TextInput>
+          <Button title="Iniciar Sesión" style={styles.button}></Button>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#312D4E",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logContainer: {
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  placeHold: {
+    marginBottom: 5,
+    backgroundColor: "#DADADA",
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "#000",
   },
 });
